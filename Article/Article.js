54ebb9927c1216004181164ff3fdd847fd634703
erica-y-chen <eyufanchen@gmail.com>
2019-03-06 13:29:53 -0800

    completed menu bar

--- Article/Article.js
+++ Article/Article.js
@@ -3,11 +3,11 @@
 class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
-    this.domElement;
+    this.domElement= domElement;
     // create a reference to the ".expandButton" class. 
-    this.expandButton;
+    this.expandButton = document.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    
+    this.expandButton.value="expand";
     // Set a click handler on the expandButton reference, calling the expandArticle method.
     
   }
@@ -26,4 +26,10 @@ class Article {
 
 */
 
-let articles;
+let articles = document.querySelectorAll('.article');
+
+articles.forEach(function(article){
+  return new Article(article);
+})
+
+console.log(articles);
